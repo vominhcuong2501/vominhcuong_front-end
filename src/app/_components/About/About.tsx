da6422efad0030981939771b34298020fa897f42
vominhcuong2501 <minhcuongvo2501@gmail.com>
2024-02-02 21:21:02 +0700

    Update code 02.02.2024

--- src/app/_components/About/About.tsx
+++ src/app/_components/About/About.tsx
@@ -3,67 +3,66 @@
 import { motion } from "framer-motion";
 
 const About = () => {
-	return (
-		<div
-			className="relative mx-auto flex lg:flex-row flex-col h-screen max-w-[1200px] items-center justify-center px-5 p5-10 lg:pt-14 text-center lg:text-left"
-			id="portfolioAbout"
-		>
-			<h2 className="md:absolute lg:top-20 z-10 translate-x-3 text-16 uppercase tracking-[20px] md:text-32 font-bold pb-2 lg:pb-0">
-				About
-			</h2>
-			<motion.div
-				initial={{
-					opacity: 0,
-				}}
-				whileInView={{
-					opacity: 1,
-				}}
-				transition={{
-					duration: 1.2,
-				}}
-				className="flex flex-col items-center justify-center gap-4 lg:gap-8 lg:flex-row"
-			>
-				<motion.img
-					initial={{
-						x: -200,
-					}}
-					whileInView={{
-						x: 0,
-					}}
-					viewport={{ once: true }}
-					transition={{
-						duration: 1,
-					}}
-					className="relative mx-auto shrink-0 object-cover lg:w-[600px] rounded-[16px]"
-					src="./cuong.jpeg"
-					alt={"About me"}
-				/>
+  return (
+    <div
+      className="relative mx-auto flex lg:flex-row flex-col h-screen max-w-[1200px] items-center justify-center px-5 pt-10 lg:pt-14 text-center lg:text-left"
+      id="portfolioAbout"
+    >
+      <h2 className="lg:absolute lg:top-20 z-10 translate-x-3 text-16 uppercase tracking-[20px] md:text-32 font-bold pb-2 lg:pb-0">
+        About
+      </h2>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        className="flex flex-col items-center justify-center gap-4 lg:gap-8 lg:flex-row"
+      >
+        <motion.img
+          initial={{
+            x: -200,
+          }}
+          whileInView={{
+            x: 0,
+          }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 1,
+          }}
+          className="relative mx-auto shrink-0 object-cover md:w-[500px] rounded-[16px]"
+          src="./cuong.jpeg"
+          alt={"About me"}
+        />
 
-				<ul className="list-disc pl-5">
-					<li className="text-14 md:text-18">
-						My name is Vo Minh Cuong, and I was born in 1996 in Ho Chi Minh
-						City, Vietnam. Currently, I am living and working in Ho Chi Minh
-						City, Vietnam as a Frontend Web Developer 🖥 with two year of
-						experience.
-					</li>
-					<li className="text-14 md:text-18">
-						I love learning new technologies and building valuable application
-						that helps others with their work. I’m considered a team-player
-						because I like to help other and tend to work well within the group.
-					</li>
-					<li className="text-14 md:text-18">
-						Short-term goal: Having a challenging job, improve the experience,
-						training working skills effectively, accurately and quickly. Improve
-						technical knowledge.
-					</li>
-					<li className="text-14 md:text-18">
-						Long-term goal: Learn and strong in many frameworks and languages.
-						Become a Fullstack Developer
-					</li>
-				</ul>
-			</motion.div>
-		</div>
-	);
+        <ul className="list-disc pl-5 md:w-[500px]">
+          <li className="text-14 md:text-18">
+            My name is Vo Minh Cuong, and I was born in 1996. Currently, I am
+            living and working in Ho Chi Minh City, Vietnam as a Frontend Web
+            Developer with two year of experience.
+          </li>
+          {/* <li className="text-14 md:text-18">
+            I love learning new technologies and building valuable application
+            that helps others with their work. I’m considered a team-player
+            because I like to help other and tend to work well within the group.
+          </li> */}
+          <li className="text-14 md:text-18">
+            Short-term goal: Having a challenging job, improve the experience,
+            training working skills effectively, accurately and quickly. Improve
+            technical knowledge.
+          </li>
+          <li className="text-14 md:text-18">
+            Long-term goal: Learn and strong in many frameworks and languages.
+            Become a Fullstack Developer
+          </li>
+        </ul>
+      </motion.div>
+    </div>
+  );
 };
 
 export default About;
--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -1,83 +1,94 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import React from 'react'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import { motion } from 'framer-motion'
+import Image from "next/image";
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
-const Hero = ({ }: Props) => {
+const Hero = ({}: Props) => {
   const [slogan] = useTypewriter({
-    words: ['Hi, The Name Vo Minh Cuong'],
+    words: ["Hi, The Name Vo Minh Cuong"],
     loop: true,
     deleteSpeed: 20,
-    typeSpeed: 50
-  })
+    typeSpeed: 50,
+  });
 
   return (
-    <div className=' flex h-screen flex-col items-center justify-center overflow-hidden pt-28 text-center'>
-      <div className='space-y-5'>
-        <div className='relative z-0'>
-          <div className='absolute left-1/2 top-1/2'>
+    <div className=" flex h-screen flex-col items-center justify-center overflow-hidden pt-28 text-center">
+      <div className="space-y-5">
+        <div className="relative z-0">
+          <div className="absolute left-1/2 top-1/2">
             <motion.div
               initial={{
-                opacity: 0
+                opacity: 0,
               }}
               animate={{
                 scale: [1, 2, 2, 1],
-                opacity: [0.5, 0.5, 1, 1]
+                opacity: [0.5, 0.5, 1, 1],
               }}
               transition={{
-                duration: 2
+                duration: 2,
               }}
-              className='relative flex items-center justify-center'
+              className="relative flex items-center justify-center"
             >
-              <div className='absolute  h-[300px] w-[300px] animate-ping rounded-full border border-neutral-500' />
-              <div className='absolute  h-[600px] w-[600px] animate-pulse rounded-full border border-neutral-500' />
+              <div className="absolute  h-[300px] w-[300px] animate-ping rounded-full border border-neutral-500" />
+              <div className="absolute  h-[600px] w-[600px] animate-pulse rounded-full border border-neutral-500" />
             </motion.div>
           </div>
           <Image
-            className='relative mx-auto h-32 w-32 rounded-full object-cover'
-            src='/cam-bong-bong.jpg'
-            alt='avatar'
+            className="relative mx-auto h-32 w-32 rounded-full object-cover"
+            src="/cam-bong-bong.jpg"
+            alt="avatar"
             width={128}
             height={128}
             priority
           />
         </div>
-        <div className='z-1 relative space-y-5'>
-          <h2 className='text-xs uppercase tracking-[8px] md:text-sm md:tracking-[12px]'>Frontend Developer</h2>
-          <h1 className='px-4 text-20 font-bold md:px-10 md:text-28'>
+        <div className="z-1 relative space-y-5">
+          <h2 className="text-xs uppercase tracking-[8px] md:text-sm md:tracking-[12px]">
+            Frontend Developer
+          </h2>
+          <h1 className="px-4 text-20 font-bold md:px-10 md:text-28">
             <span>{slogan}</span>
-            <Cursor cursorColor='#ea580c' />
+            <Cursor cursorColor="#ea580c" />
           </h1>
-          <div className='z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm '>
-            <a href='#portfolioAbout' className='transition-all duration-300 hover:text-[#e60000]'>
-              <button className='uppercase' title='About' aria-label='About'>
-                About
-              </button>
+
+          <div className="z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm ">
+            <a
+              href="#portfolioAbout"
+              className="transition-all duration-300 hover:text-[#e60000] uppercase"
+              title="About"
+            >
+              About
             </a>
-            <a href='#portfolioExperience' className='transition-all duration-300 hover:text-[#e60000]'>
-              <button className='uppercase' title='Experience' aria-label='Experience'>
-                Experience
-              </button>
+            <a
+              href="#portfolioExperience"
+              className="transition-all duration-300 hover:text-[#e60000] uppercase"
+              title="Experience"
+            >
+              Experience
             </a>
-            <a href='#portfolioSkills' className='transition-all duration-300 hover:text-[#e60000]'>
-              <button className='uppercase' title='Skills' aria-label='Skills'>
-                Skills
-              </button>
+            <a
+              href="#portfolioSkills"
+              className="transition-all duration-300 hover:text-[#e60000] uppercase"
+              title="Skills"
+            >
+              Skills
             </a>
-            <a href='#portfolioProjects' className='transition-all duration-300 hover:text-[#e60000]'>
-              <button className='uppercase' title='Projects' aria-label='Projects'>
-                Projects
-              </button>
+            <a
+              href="#portfolioProjects"
+              className="transition-all duration-300 hover:text-[#e60000] uppercase"
+              title="Projects"
+            >
+              Projects
             </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
