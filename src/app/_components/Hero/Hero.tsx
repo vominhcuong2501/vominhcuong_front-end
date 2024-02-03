afda09ac087e41dc4c6a8bb662e874e68c6b3fd5
vominhcuong2501 <minhcuongvo2501@gmail.com>
2024-02-03 18:45:08 +0700

    Fix onclick go to slide

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -5,109 +5,100 @@ import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { motion } from "framer-motion";
 
-type Props = {};
+type Props = {
+  handleClick: (slideIndex: any) => void;
+};
 
-const Hero = ({}: Props) => {
-	const [slogan] = useTypewriter({
-		words: ["Hi, The Name Vo Minh Cuong"],
-		loop: true,
-		deleteSpeed: 20,
-		typeSpeed: 50,
-	});
+const Hero = ({ handleClick }: Props) => {
+  const [slogan] = useTypewriter({
+    words: ["Hi, My name's Vo Minh Cuong"],
+    loop: true,
+    deleteSpeed: 20,
+    typeSpeed: 50,
+  });
 
-	return (
-		<div
-			className=" flex h-screen flex-col items-center justify-center overflow-hidden pt-28 text-center "
-			id="portfolioHero"
-		>
-			<div className="space-y-5 ">
-				<div className="relative z-0">
-					<div className="absolute left-1/2 top-1/2">
-						<motion.div
-							initial={{
-								opacity: 0,
-							}}
-							animate={{
-								scale: [1, 2, 2, 1],
-								opacity: [0.5, 0.5, 1, 1],
-							}}
-							transition={{
-								duration: 2,
-							}}
-							className="relative flex items-center justify-center"
-						>
-							<div className="absolute  h-[300px] w-[300px] animate-ping rounded-full border border-white" />
-							<div className="absolute  h-[600px] w-[600px] animate-pulse rounded-full border border-white" />
-						</motion.div>
-					</div>
-					<Image
-						className="relative mx-auto h-32 w-32 rounded-full object-cover"
-						src="/cam-bong-bong.jpg"
-						alt="avatar"
-						width={128}
-						height={128}
-						priority
-					/>
-				</div>
-				<div className="z-1 relative space-y-5 text-white">
-					<h2 className="text-xs uppercase tracking-[8px] md:text-sm md:tracking-[12px]">
-						Frontend Developer
-					</h2>
-					<h1 className="px-4 text-20 font-bold md:px-10 md:text-28">
-						<span>{slogan}</span>
-						<Cursor cursorColor="#ea580c" />
-					</h1>
-
-					<div className="z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm ">
-						<a
-							href="#portfolioAbout"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="About"
-						>
-							About
-						</a>
-						<a
-							href="#portfolioExperience"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="Experience"
-						>
-							Experience
-						</a>
-						<a
-							href="#portfolioSkills"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="Skills"
-						>
-							Skills
-						</a>
-						<a
-							href="#portfolioProjects"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="Projects"
-						>
-							Projects
-						</a>
-					</div>
-					<div className="z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm ">
-						<a
-							href="#portfolioContact"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="Contact"
-						>
-							Contact
-						</a>
-						<a
-							href="#portfolioEducation"
-							className="transition-all duration-300 hover:text-[#e60000] uppercase font-semibold"
-							title="Education"
-						>
-							Education
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="text-center w-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 "
+      id="portfolioHero"
+    >
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          scale: [1, 2, 2, 1],
+          opacity: [0.5, 0.5, 1, 1],
+        }}
+        transition={{
+          duration: 2,
+        }}
+        className="space-y-5 text-white"
+      >
+        <div className="hidden md:block border border-white h-[550px] w-[550px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+        <Image
+          className="relative mx-auto h-40 w-40 rounded-full object-cover object-top"
+          src="/cam-bong-bong.jpg"
+          alt="avatar"
+          width={949}
+          height={1078}
+          priority
+        />
+        <h2 className="text-xs text-center uppercase tracking-[8px] md:text-sm ">
+          Frontend Developer
+        </h2>
+        <h1 className="px-4 text-20 font-bold md:px-10 md:text-28 text-gradient-red text-center h-10 w-full">
+          {slogan}
+        </h1>
+        <div className="z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm ">
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="About"
+            onClick={() => handleClick(1)}
+          >
+            About
+          </p>
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="Experience"
+            onClick={() => handleClick(2)}
+          >
+            Experiences
+          </p>
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="Skills"
+            onClick={() => handleClick(3)}
+          >
+            Skills
+          </p>
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="Projects"
+            onClick={() => handleClick(4)}
+          >
+            Projects
+          </p>
+        </div>
+        <div className="z-1 relative mx-auto flex justify-center gap-6 text-xs md:gap-10 md:text-sm ">
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="Education"
+            onClick={() => handleClick(5)}
+          >
+            Education
+          </p>
+          <p
+            className="transition-all duration-300 hover:text-[#22bcc3] uppercase font-semibold cursor-pointer"
+            title="Contact"
+            onClick={() => handleClick(6)}
+          >
+            Contact
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,84 @@
-// "use client";
-// import About from "./_components/About";
-// import Hero from "./_components/Hero";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import { Mousewheel, Pagination } from "swiper/modules";
-// import Experience from "./_components/Experience";
-// import Project from "./_components/Project";
-// import Skills from "./_components/Skills";
-
-// export default function Home() {
-// 	const menuPagination = [
-// 		"Home",
-// 		"About",
-// 		"Experience",
-// 		"Skills",
-// 		"Projects",
-// 		"Contact",
-// 		"Education",
-// 	];
-
-// 	const pagination = {
-// 		clickable: true,
-// 		renderBullet: (index: number) => {
-// 			return `<p class="!text-white !border !border-white !p-2 !my-2 !rounded-[4px] !z-50 font-bold swiper-pagination-bullet custom-pagination">${menuPagination[index]}</p>`;
-// 		},
-// 	};
-
-// 	return (
-// 		<section>
-// 			<Swiper
-// 				direction={"vertical"}
-// 				slidesPerView={1}
-// 				spaceBetween={0}
-// 				mousewheel={true}
-// 				modules={[Mousewheel, Pagination]}
-// 				className="parentSwiper h-[100vh]"
-// 				pagination={pagination}
-// 			>
-// 				<SwiperSlide>
-// 					<Hero />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<About />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<Experience />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<Skills />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<Project />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<Project />
-// 				</SwiperSlide>
-// 				<SwiperSlide>
-// 					<Project />
-// 				</SwiperSlide>
-// 			</Swiper>
-// 		</section>
-// 	);
-// }
-
+"use client";
 import About from "./_components/About";
 import Hero from "./_components/Hero";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Mousewheel } from "swiper/modules";
 import Experience from "./_components/Experience";
 import Project from "./_components/Project";
 import Skills from "./_components/Skills";
+import React, { useRef, useState } from "react";
+import "swiper/swiper-bundle.css";
 
 export default function Home() {
-	return (
-		<section>
-			<Hero />
+  // Kích hoạt các module cần thiết của Swiper
+  const swiperRef = useRef(null);
+  // State để lưu trạng thái hiện tại của Swiper
+  const [swiper, setSwiper] = useState<any>();
 
-			<About />
+  // Hàm xử lý khi nhấn vào thẻ để điều hướng đến slide cụ thể
+  const goToSlide = (slideIndex: any) => {
+    swiper.slideTo(slideIndex);
+  };
 
-			<Experience />
-
-			<Skills />
-
-			<Project />
-
-			<Project />
-
-			<Project />
-		</section>
-	);
+  return (
+    <section className="max-w-[1200px] mx-auto relative">
+      <button
+        className=" absolute right-10 bottom-10 z-10 bg-white cursor-pointer rounded-full p-4 rotate-90"
+        onClick={() => goToSlide(0)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
+          fill="none"
+        >
+          <path
+            d="M9.28608 2.19582C9.00097 1.9347 8.54129 1.9347 8.25617 2.19582L3.42085 6.62428C3.19392 6.83212 3.19392 7.16785 3.42085 7.37568L8.25617 11.8041C8.54129 12.0653 9.00097 12.0653 9.28608 11.8041C9.5712 11.543 9.5712 11.122 9.28608 10.8609L5.07335 6.99732L9.2919 3.13374C9.5712 2.87795 9.5712 2.45162 9.28608 2.19582Z"
+            fill="#E60000"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.39865 11.927C9.04983 12.2465 8.49243 12.2465 8.14361 11.927L3.30828 7.49859C3.00922 7.22469 3.00922 6.77527 3.30828 6.50137L8.14361 2.07291C8.49222 1.75364 9.04916 1.75345 9.39802 2.07234C9.75602 2.39417 9.75598 2.93471 9.40447 3.25665L5.32003 6.9974L9.39865 10.738C9.39862 10.738 9.39868 10.738 9.39865 10.738C9.75581 11.0652 9.75587 11.5999 9.39865 11.927ZM9.28608 2.19582L9.17351 2.31873C8.9521 2.11595 8.59015 2.11595 8.36874 2.31873L3.53341 6.74719C3.37862 6.88896 3.37862 7.111 3.53341 7.25277L8.36874 11.6812C8.59015 11.884 8.9521 11.884 9.17351 11.6812C9.3865 11.4862 9.3865 11.1789 9.17351 10.9838L4.82668 6.99723L9.17933 3.01083C9.38621 2.82137 9.38637 2.50971 9.17478 2.31988L9.28608 2.19582Z"
+            fill="#E60000"
+          />
+        </svg>
+      </button>
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={1}
+        spaceBetween={0}
+        mousewheel={true}
+        modules={[Mousewheel]}
+        className="parentSwiper h-[100vh]"
+        ref={swiperRef}
+        onSwiper={(swiper: any) => setSwiper(swiper)}
+      >
+        <SwiperSlide>
+          <Hero handleClick={goToSlide} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <About />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Experience />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Skills />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Project />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 }
