05c8d6743c0bc3f33b2beefc8df7889bc71c8b2d
VÕ MINH CƯỜNG <102784644+vominhcuong2501@users.noreply.github.com>
2024-02-05 11:39:15 +0700

    05.02.2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import Skills from "./_components/Skills";
 import React, { useRef, useState } from "react";
 import "swiper/swiper-bundle.css";
 import Education from "./_components/Education";
+import Contact from "./_components/Contact";
 
 export default function Home() {
 	// Kích hoạt các module cần thiết của Swiper
@@ -24,27 +25,21 @@ export default function Home() {
 	};
 
 	return (
-		<section className="max-w-[1200px] mx-auto ">
+		<section className="max-w-[1920px] mx-auto ">
 			<button
-				className=" absolute right-2 bottom-2 md:right-10 md:bottom-10 z-10 bg-white cursor-pointer rounded-full p-4 rotate-90"
+				className=" absolute right-2 bottom-2 md:right-10 md:bottom-10 z-10 bg-white cursor-pointer rounded-full p-4 hover:opacity-90"
 				onClick={() => goToSlide(0)}
 			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="14"
-					height="14"
-					viewBox="0 0 14 14"
+					width="16"
+					height="16"
+					viewBox="0 0 16 16"
 					fill="none"
 				>
 					<path
-						d="M9.28608 2.19582C9.00097 1.9347 8.54129 1.9347 8.25617 2.19582L3.42085 6.62428C3.19392 6.83212 3.19392 7.16785 3.42085 7.37568L8.25617 11.8041C8.54129 12.0653 9.00097 12.0653 9.28608 11.8041C9.5712 11.543 9.5712 11.122 9.28608 10.8609L5.07335 6.99732L9.2919 3.13374C9.5712 2.87795 9.5712 2.45162 9.28608 2.19582Z"
-						fill="#E60000"
-					/>
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="M9.39865 11.927C9.04983 12.2465 8.49243 12.2465 8.14361 11.927L3.30828 7.49859C3.00922 7.22469 3.00922 6.77527 3.30828 6.50137L8.14361 2.07291C8.49222 1.75364 9.04916 1.75345 9.39802 2.07234C9.75602 2.39417 9.75598 2.93471 9.40447 3.25665L5.32003 6.9974L9.39865 10.738C9.39862 10.738 9.39868 10.738 9.39865 10.738C9.75581 11.0652 9.75587 11.5999 9.39865 11.927ZM9.28608 2.19582L9.17351 2.31873C8.9521 2.11595 8.59015 2.11595 8.36874 2.31873L3.53341 6.74719C3.37862 6.88896 3.37862 7.111 3.53341 7.25277L8.36874 11.6812C8.59015 11.884 8.9521 11.884 9.17351 11.6812C9.3865 11.4862 9.3865 11.1789 9.17351 10.9838L4.82668 6.99723L9.17933 3.01083C9.38621 2.82137 9.38637 2.50971 9.17478 2.31988L9.28608 2.19582Z"
-						fill="#E60000"
+						d="M0.705315 7.29468C0.31578 7.68422 0.31578 8.31578 0.705316 8.70532C1.0946 9.0946 1.72568 9.09489 2.11531 8.70595L7 3.83V15C7 15.5523 7.44772 16 8 16C8.55229 16 9 15.5523 9 15V3.83L13.8703 8.70905C14.2622 9.10159 14.8981 9.10188 15.2903 8.70968C15.6823 8.31774 15.6823 7.68226 15.2903 7.29032L9.33333 1.33333C8.59695 0.596952 7.40305 0.596954 6.66667 1.33333L0.705315 7.29468Z"
+						fill="#e60000"
 					/>
 				</svg>
 			</button>
@@ -77,7 +72,7 @@ export default function Home() {
 					<Education />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Education />
+					<Contact />
 				</SwiperSlide>
 			</Swiper>
 		</section>
